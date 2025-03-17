4b7b4e04550786a619eaa6e565a41f16347149fb
FOTIE FOTIE NATHANAEL <kenryalonzo@gmail.com>
2025-03-17 02:23:48 +0100

    The Logic is Closed -> localStorage

--- src/components/taskInput/taskInput.jsx
+++ src/components/taskInput/taskInput.jsx
@@ -1,15 +1,33 @@
 // Ce composant est utilisé pour afficher le champ de saisie de tâche.
+import { useState } from "react";
 import styles from "./TaskInput.module.css";
 
-export const TaskInput = () => {
+export const TaskInput = ({addTask}) => {
+
+  const [taskTitle, setTaskTitle] = useState('');
+
+  const handleInputChange = (event) => {
+    setTaskTitle(event.target.value);
+  }
+
+  const handleAddTask = (event) => {
+    event.preventDefault();
+    if (taskTitle.trim()) {
+      addTask(taskTitle);
+      setTaskTitle('');
+    }
+  }
+
   return (
     <div className={`box ${styles.element}`}>
       <h2 className={styles.title}>🎯 ajoute ta prochaine tache</h2>
-      <form className={styles.container}>
+      <form className={styles.container} onSubmit={handleAddTask}>
         <input 
           type="text" 
           className={styles.input}
           placeholder="Indiquer un titre de tache explicite."
+          onChange={handleInputChange}
+          value={taskTitle}
         />
         <button className="button-primary" type="submit">
           Ajouter
--- src/components/taskList/taskList.jsx
+++ src/components/taskList/taskList.jsx
@@ -2,13 +2,49 @@
 import styles from "./TaskList.module.css";
 import { TaskItem } from "../taskItem/taskItem";
 
-export const TaskList = () => {
+export const TaskList = ({
+  tasksList,
+  uncompletedTasks,
+  editTask,
+  deleteTask,
+}) => {
+
+  const tasKList = tasksList.map((task) => (
+    <TaskItem 
+      key={task.id} 
+      task={task} 
+      editTask={editTask} 
+      deleteTask={deleteTask} 
+    />
+  ));
+
+  if (tasKList && tasKList.length > 0) {
+    return (
+      <div className="box">
+
+        <h2 className={styles.title}>
+          {uncompletedTasks > 0 && (
+              <>📝 Il te reste encore <span className="important">{uncompletedTasks}</span> tâches à accomplir</>
+          )}
+
+          {uncompletedTasks === 0 && (
+            <> 🤝 Genial, tu as accompli toutes tes taches !</>
+          )}
+        </h2>
+
+        {tasksList && tasksList.length > 0 && (
+          <ul className={styles.container}>
+            {tasKList}
+          </ul>
+        )}
+      </div>
+    );
+  }
   return (
     <div className="box">
-      <h2 className={styles.title}>
-        📝 Il te reste encore x tâches à accomplir
+      <h2 className={styles.emptyState}>
+        👋 Salut, Rien a faire pour le moment ! profite de ton temps libre.
       </h2>
-      <TaskItem />
     </div>
   );
 };
